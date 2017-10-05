82e5c45479a902880303af5e0d8320a0b5aa5035
Manuel Schmalstieg <git.code@ms-studio.net>
2017-10-05 22:10:04 +0200

    graphisme du module Next Level

--- nextlevel.js
+++ nextlevel.js
@@ -1,45 +1,52 @@
 /*!
  * NextLevel JS
- * 
+ *
  * Released under the Unlicense
  */
 
 // Faire un timeout après XXX secondes de jeu:
 
 function TimeRunningOut() {
-	
+
 	var interval = 3000;
-	
+
 	setTimeout( function(){
-	
+
                   NextLevel();
-    
+
     },interval);
-	
+
 }
 
 // exécuter la fonction au chargement:
 
 window.onload = TimeRunningOut;
- 
+
 // Créer un élément HTML qui permet d'avancer au prochain niveau:
 
 function NextLevel() {
-	
-	console.log("Next Level!");
-	
-	// construire l'élément:
 
-	// au click, l'élément renvoie à random.htmlvar LevelLink = '<a href="'+L+'">'+L+'</a>';
-    var box = document.createElement('div');
-    box.style.position = 'absolute';
-    box.style.width = '6em';
-    box.style.height = '1.5em';
-    box.style.bottom = '10px';
-    box.style.right = '10px';
-    box.style.zIndex = '10';
-    box.style.background = 'white';
-    box.innerHTML = '<a href="../random.html">NEXT</a>';
+	console.log("Next Level!");
+
+	// construire l'élément:
+	// au click, l'élément renvoie à random.html
+
+		var nextstyle = document.createElement('link');
+		nextstyle.setAttribute('rel', 'stylesheet');
+		nextstyle.setAttribute('href', '../nextlevel.css');
+		document.querySelector("head").appendChild(nextstyle);
+
+		var box = document.createElement('div');
+		box.id = 'nextlevel';
+    box.innerHTML = '<a href="#" id="replay">REPLAY</a><a id="next" href="../random.html">NEXT</a>';
     document.querySelector("body").appendChild(box);
-	
+
 }
+
+// Action Replay: recharger la page courante
+
+document.querySelector("button").addEventListener( "click", function(){
+
+ 
+});
+
